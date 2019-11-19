fd56373200e9781067bc702cbf0402b899560200
diekus <diekus@gmail.com>
2019-11-19 18:58:02 +0000

    adds sticky title to template
    -Adds sticky title and intersection observer to set a title once the main description scrolls up

---
+++ js/_gg_ui.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', () => {
+    /*Sticky header global app*/
+
+    let _page_title = document.querySelector('#_page_title');
+
+    let observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.intersectionRatio > 0) {
+                document.querySelector('._sticky_title').classList.add('_sticky_title_disappear');
+                document.querySelector('._sticky_title').classList.remove('_sticky_title_appear');
+                console.log('title is in');
+            } else {
+                document.querySelector('._sticky_title').classList.add('_sticky_title_appear');
+                document.querySelector('._sticky_title').classList.remove('_sticky_title_disappear');
+                console.log('title is out');
+            }
+        });
+        console.log(entries);
+    });
+
+    observer.observe(_page_title);
+});
+
