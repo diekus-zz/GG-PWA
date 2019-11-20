83b687bfe49f0484a64540aa5acf54c5e4e4d761
diekus <diekus@gmail.com>
2019-11-20 13:21:01 +0000

    base for observer cards

--- js/_gg_ui.js
+++ js/_gg_ui.js
@@ -1,4 +1,4 @@
-// If `prefers-color-scheme` is not supported, fall back to light mode.
+/*prefers-color-scheme support on older browser*/
 if (window.matchMedia('(prefers-color-scheme: dark)').media === 'not all') {
     document.documentElement.style.display = 'none';
     document.head.insertAdjacentHTML(
@@ -23,5 +23,44 @@ window.addEventListener('load', () => {
         });
     });
     observer.observe(_page_title);
+
+
+    /*cards animation entrance*/
+    const lis = Array.from(document.querySelectorAll('li'));
+    const intersectingEvent = new Event('intersecting');
+
+    const intersectionObserverOptions = {
+        threshold: 0.25
+    }
+
+    let observer = new IntersectionObserver(entries => entries.forEach(function onIntersection({
+            isIntersecting,
+            target
+        }) {
+            if (isIntersecting) {
+                target.dispatchEvent(intersectingEvent);
+            }
+        }), intersectionObserverOptions);
+
+    lis.forEach(li => {
+        observer.observe(li);
+
+        // Have the box on the screen but invisible at the start
+        // Since it needs to be detected by the intersection observer
+        li.style.opacity = 0;
+
+        // Only once when it is intersecting do the animation and make it visible
+        li.addEventListener('intersecting', () => {
+            const animation = li.animate(
+            {'transform': ['translateX(100vw)','translateX(0)']},
+            {easing: getComputedStyle(document.documentElement).getPropertyValue('--easeInCubic'),
+             duration: 700,
+             fill: 'forwards'
+            });
+            li.style.opacity = 1;
+        }, {
+            once: true
+        });
+    });
 });
 
